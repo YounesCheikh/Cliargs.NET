37e270c17a8fc6ecf33a4e08dfca893b7611fdab
Younes.Cheikh <younes.cheikh@gmail.com>
2022-02-18 23:44:44 +0100

    Added RangeRule tests

--- src/Cliargs.Tests/Rules/RangeValidationRuleTests.cs
+++ src/Cliargs.Tests/Rules/RangeValidationRuleTests.cs
@@ -1,12 +1,48 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Cliargs.Rules;
 namespace Cliargs.Tests;
 
 [TestClass]
 public class RangeValidationRuleTests {
-    
+
+    enum FakeEnum
+    {
+        ElementOne,
+        ElementTwo,
+        ElementThree
+    }
+
     [TestMethod]
-    public void SampleRangeTest() {
-        
+    public void SampleIntRangeSuccessTest() {
+        RangeValidationRule<int?> rule = new RangeValidationRule<int?>(new int?[] { 1, 2, 3 });
+        Assert.IsTrue(rule.IsValid(1));
+        Assert.IsTrue(rule.IsValid(2));
+        Assert.IsTrue(rule.IsValid(3));
+        Assert.IsFalse(rule.IsValid(0));
+        Assert.IsFalse(rule.IsValid(4));
+        Assert.IsFalse(rule.IsValid(-1));
+        Assert.IsFalse(rule.IsValid(null));
+    }
+
+    [TestMethod]
+    public void SampleStringRangeSuccessTest()
+    {
+        RangeValidationRule<string?> rule = new RangeValidationRule<string?>(new string?[] { true.ToString().ToLower(), false.ToString().ToLower() });
+        Assert.IsTrue(rule.IsValid("true".ToLower()));
+        Assert.IsTrue(rule.IsValid("false".ToLower()));
+        Assert.IsFalse(rule.IsValid("yes"));
+        Assert.IsFalse(rule.IsValid("no"));
+        Assert.IsFalse(rule.IsValid(""));
+        Assert.IsFalse(rule.IsValid(default));
+    }
+
+    [TestMethod]
+    public void SampleEnumRangeSuccessTest()
+    {
+        RangeValidationRule<FakeEnum?> rule = new RangeValidationRule<FakeEnum?>(new FakeEnum?[] { FakeEnum.ElementOne, FakeEnum.ElementThree });
+        Assert.IsTrue(rule.IsValid(FakeEnum.ElementOne));
+        Assert.IsTrue(rule.IsValid(FakeEnum.ElementThree));
+        Assert.IsFalse(rule.IsValid(FakeEnum.ElementTwo));
+        Assert.IsFalse(rule.IsValid(null));
     }
 } 
